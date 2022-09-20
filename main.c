7973194a4b25be3b2f3379525d33406df0eb5619
eunnbyeol <113367529+eunnbyeol@users.noreply.github.com>
2022-09-20 18:45:26 +0900

    04

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	char text;
+	int a,b;
 	
-	printf("input a character:");
-	scanf("%c", &text);
+	printf("분자를 입력하세요.");
+	scanf("%d", &a);
 	
-	printf("the next character of %c(&d) is %c(%d)\n", text, text, text+1, text+1);
-    
+	printf("분모를 입력하세요.");
+	scanf("%d", &b);
+	
 	return 0;
 }
 
